6d53949399f8d85b9cb1d92d702669f46bf5a9d2
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-11-23 16:47:56 +0200

    added basic Sign Up form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule {}
