96a97f0d4d76008fcff88c091d7b6eba417ce990
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-11-26 18:43:10 +0200

    added current-training

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.progress += 5;
+    }, 1000)
   }
 
 }
