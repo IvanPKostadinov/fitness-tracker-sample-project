b0362967c4acf2ccde344f0dce7733b017b360f0
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-11-28 17:38:02 +0200

    added pagination to past-trainings.component data table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -16,8 +17,9 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   // array of the type, that we've passed -> here Execise[]:
   dataSource = new MatTableDataSource<Exercise>();
 
-  // Here we wire-up the sorting:
+  // Here we wire-up the sorting and filtering:
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) { }
 
@@ -28,8 +30,9 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // Here we wire-up the sorting:
+    // Here we wire-up the sorting and filtering:
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(event: any) {
