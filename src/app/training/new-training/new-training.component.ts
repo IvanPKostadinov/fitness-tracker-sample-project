0d23c28e3cdd7dfe95e205d5d84a57b01778b235
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-12-07 10:27:58 +0200

    added AngularFirestore from Angularfire to new-training.component.ts

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -7,17 +9,28 @@ import { TrainingService } from '../training.service';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
   // @Output() trainingStart = new EventEmitter<void>();
-  exercises: Exercise[] = [];
+  // exercises: Exercise[] = [];
+  // We put <any> here because Exercise has an id property:
+  exercises: Observable<any>;
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(
+    private trainingService: TrainingService,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
     // It's good practice to initialize some services in the ngOnInit():
-    this.exercises = this.trainingService.getAvailableExercises();
+    // this.exercises = this.trainingService.getAvailableExercises();
+
+    // Here we use Angularfire to reach out to Firebase:
+    this.exercises = this.db.collection('availableExercises').valueChanges();
+    // .subscribe((result) => {
+    //   console.log(result)
+    // });
   }
 
   onStartTraining(form: NgForm) {
