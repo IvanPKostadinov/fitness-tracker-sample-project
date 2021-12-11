01155e90b3bb7cf85a29044845d4b2950fd706bd
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-12-11 14:52:34 +0200

    fetch completed or canceled exercises from Firebase in past-trainings.component.ts

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,50 +1,58 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, DocumentChangeAction } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  DocumentChangeAction,
+} from '@angular/fire/compat/firestore';
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Exercise } from './exercise.model';
 
 // to be able to inject something in a Service, we need to add @Injectable()
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
   exercisesChanged = new Subject<Exercise[]>();
+  finishedExercisesChanged = new Subject<Exercise[]>();
   private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
-  private exercises: Exercise[] = [];
+  // private finishedExercises: Exercise[] = [];
 
   constructor(private db: AngularFirestore) {}
 
   fetchAvailableExercises() {
     // Here we use Angularfire to reach out to Firebase:
     // this.exercises = this.db.collection('availableExercises').valueChanges();
-    return this.db
-      .collection('availableExercises')
-      // .snapshotChanges() returns an Observable!
-      .snapshotChanges()
-      .pipe(
-        map((docArray: DocumentChangeAction<any>[]) => {
-          return docArray.map(doc => {
-            return {
-              id: doc.payload.doc.id,
-              // With res.payload.doc.data() -> we can extract the data from Firebase!!!
-              ...doc.payload.doc.data(),
-            }
+    return (
+      this.db
+        .collection('availableExercises')
+        // .snapshotChanges() returns an Observable!
+        .snapshotChanges()
+        .pipe(
+          map((docArray: DocumentChangeAction<any>[]) => {
+            return docArray.map((doc) => {
+              return {
+                id: doc.payload.doc.id,
+                // With res.payload.doc.data() -> we can extract the data from Firebase!!!
+                ...doc.payload.doc.data(),
+              };
+            });
           })
+        )
+        .subscribe((exercises: Exercise[]) => {
+          this.availableExercises = exercises;
+          this.exercisesChanged.next([...this.availableExercises]);
         })
-      )
-      .subscribe((exercises: Exercise[]) => {
-        this.availableExercises = exercises;
-        this.exercisesChanged.next([...this.availableExercises]);
-      });
+    );
   }
 
   startExercise(selectedId: string) {
-    this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.runningExercise = this.availableExercises.find(
+      (ex) => ex.id === selectedId
+    );
     // We pass (with next()) the selected exercise through a Subject and subscribe to it, where we need it:
     // here we pass a copy of the runningExercise:
-    this.exerciseChanged.next({...this.runningExercise});
+    this.exerciseChanged.next({ ...this.runningExercise });
   }
 
   completeExercise() {
@@ -73,8 +81,14 @@ export class TrainingService {
     return { ...this.runningExercise };
   }
 
-  getCompletedOrCancelledExercises() {
-    return this.exercises.slice();
+  fetchCompletedOrCancelledExercises() {
+    // valueChanges gives us an array of elements WITHOUT id:
+    this.db
+      .collection('finishedExercises')
+      .valueChanges()
+      .subscribe((exercises: Exercise[]) => {
+        this.finishedExercisesChanged.next(exercises);
+      });
   }
 
   private addDataToDatabase(exercise: Exercise) {
