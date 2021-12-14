1905c11f3b207818fbfd105a05f8d387856f9cff
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-12-14 18:20:58 +0200

    Implemented Lazy Loading for training-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TrainingComponent } from './training/training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
+  /**
+   *  !!! We implement Lazy Loading with loadChildren !!!
+   * */
+  { path: 'training', loadChildren: () => import('./training/training.module').then(module => module.TrainingModule) }
 ];
 
 @NgModule({
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,14 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
+
 import { SharedModule } from '../shared/shared.module';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
+import { TrainingRoutingModule } from './training-routing.module';
 import { TrainingComponent } from './training.component';
 
 @NgModule({
@@ -22,6 +20,7 @@ import { TrainingComponent } from './training.component';
   imports: [
     SharedModule,
     AngularFirestoreModule,
+    TrainingRoutingModule,
     // CommonModule,
     // FormsModule,
     // MaterialModule,
